4a88a6561499723e41e8a8ffcd0a50deecc8d6c8
YevgeniyDev <yevgeniy.dikun@nu.edu.kz>
2025-12-22 16:49:06 +0500

    render db

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -2,6 +2,7 @@ import os
 
 DATABASE_URL = os.getenv(
     "DATABASE_URL",
+    "postgresql://cececo_bd_user:XhcXQlmQDQ7emoWSxRWpF5rF5DhvA8Nj@dpg-d54iuebe5dus73bkbt6g-a/cececo_bd",
     "postgresql+psycopg://cececo:cececo@localhost:5432/cececo"
 )
 JWT_SECRET = os.getenv("JWT_SECRET", "dev-secret-change-me")
--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -1,6 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from app.core.config import DATABASE_URL
+import os
+
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql://cececo_bd_user:XhcXQlmQDQ7emoWSxRWpF5rF5DhvA8Nj@dpg-d54iuebe5dus73bkbt6g-a/cececo_bd",
+    "postgresql+psycopg://cececo:cececo@localhost:5432/cececo"
+)
 
 engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
