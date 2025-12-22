66b7aa713ad2d8c23348e98ba120a63a1c9bbc06
YevgeniyDev <yevgeniy.dikun@nu.edu.kz>
2025-12-22 17:59:25 +0500

    cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -18,8 +18,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "http://127.0.0.1:3000",
-        "https://cececo-hub-1.onrender.com/",
+        "https://cececo-hub-1.onrender.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
